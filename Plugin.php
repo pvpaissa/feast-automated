84d6b4495e25ec9cb404dfbfe77268943d2e9f0b
Paul E. Lovato <plovato@gmail.com>
2019-11-12 14:07:52 -0700

    Updated registerSchedule and sitesearch gateways (boot).

--- Plugin.php
+++ Plugin.php
@@ -6,14 +6,11 @@ use Event;
 use DateTime;
 use System\Classes\PluginBase;
 
-use Cleanse\Feast\Models\FeastSettings;
 use Cleanse\Feast\Classes\Scheduler;
+use Cleanse\Feast\Classes\FeastSearchProvider;
 
 class Plugin extends PluginBase
 {
-    public $solo;
-    public $lp;
-
     public function pluginDetails()
     {
         return [
@@ -33,22 +30,8 @@ class Plugin extends PluginBase
 
     public function boot()
     {
-        Event::listen('offline.sitesearch.query', function ($query) {
-            $items = Party::where('name', 'like', "%${query}%")
-                ->get();
-            $results = $items->map(function ($item) use ($query) {
-                $relevance = mb_stripos($item->name, $query) !== false ? 2 : 1;
-                return [
-                    'title' => $item->name,
-                    'text' => $item->dc_group,
-                    'url' => '/light-party/profile/' . $item->lodestone,
-                    'relevance' => $relevance,
-                ];
-            });
-            return [
-                'provider' => 'Feast',
-                'results' => $results,
-            ];
+        Event::listen('offline.sitesearch.extend', function ($query) {
+            return [new FeastSearchProvider('solo'), new FeastSearchProvider('party')];
         });
     }
 
@@ -65,34 +48,33 @@ class Plugin extends PluginBase
     {
         $year = substr($yearWeek, 0, -2);
         $week = substr($yearWeek, 4);
+
         $date = new DateTime();
         $date->setISODate($year, $week);
+
         return $date->format('Y-m-d');
     }
 
-//    public function registerSchedule($schedule)
-//    {
-//        $this->solo = FeastSettings::where('season', 'solo')->first();
-//        $this->lp = FeastSettings::where('season', 'party')->first();
-//
-//        $schedule->call(function () {
-//            $getSolo = new Scheduler;
-//            $getSolo->checkLodestone('solo', $this->solo);
-//        })->cron('3 4 * * *');
-//
-//        $schedule->call(function () {
-//            $getLP = new Scheduler;
-//            $getLP->calculateRankings('solo', $this->solo);
-//        })->cron('33 4 * * *');
-//
-//        $schedule->call(function () {
-//            $getLP = new Scheduler;
-//            $getLP->checkLodestone('party', $this->lp);
-//        })->cron('3 5 * * *');
-//
-//        $schedule->call(function () {
-//            $getLP = new Scheduler;
-//            $getLP->calculateRankings('party', $this->lp);
-//        })->cron('33 5 * * *');
-//    }
+    public function registerSchedule($schedule)
+    {
+        $schedule->call(function () {
+            $getSolo = new Scheduler('solo');
+            $getSolo->checkLodestone();
+        })->cron('3 4 * * *');
+
+        $schedule->call(function () {
+            $getLP = new Scheduler('solo');
+            $getLP->calculateRankings();
+        })->cron('33 4 * * *');
+
+        $schedule->call(function () {
+            $getLP = new Scheduler('party');
+            $getLP->checkLodestone();
+        })->cron('3 5 * * *');
+
+        $schedule->call(function () {
+            $getLP = new Scheduler('party');
+            $getLP->calculateRankings();
+        })->cron('33 5 * * *');
+    }
 }
