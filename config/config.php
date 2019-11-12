1afaa11fb516cea717b71db014adb3d45c568873
Paul E. Lovato <plovato@gmail.com>
2019-11-12 15:18:03 -0700

    Temp location for config.

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+//Legact. todo: move to database!
+return [
+    'season' => 14,
+    'solo_mini_take' => 5,
+    'solo_full_take' => 50,
+    'party_mini_take' => 5,
+    'party_full_take' => 50,
+];
